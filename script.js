e577eca5ac17d34820982dda24fa3f62c5a530ae
Pratima_Tandekar <116260245+pratimatandekar@users.noreply.github.com>
2023-12-27 20:11:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleManu(){
+    const menu=document.querySelector  (".manu-links");
+    const icon=document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
