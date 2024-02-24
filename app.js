cf14ca7585754e0d8d6824dae08870945f3c0ce9
Pratima_Tandekar <116260245+pratimatandekar@users.noreply.github.com>
2024-02-24 18:44:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-let xhr, timeZoneList, timeZoneName;
-let endPoint = "http://worldtimeapi.org/api/timezone/";
-function loadTimeZones() {
-  xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = processTimeZones;
-  xhr.open("GET", endPoint, true);
-  xhr.send(null);
+let ajaxreq = new XMLHttpRequest();
+let endPoint = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+
+function connect() {
+  ajaxreq.onreadystatechange = showTime;
+  ajaxreq.open("get", endPoint, true);
+  ajaxreq.send(null);
 }
-function processTimeZones() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const timeZoneArr = JSON.parse(xhr.responseText);
-    timeZoneList = document.getElementById("timezone");
-    let items = "";
-    timeZoneArr.forEach((t) => {
-      items += `<option>${t}</option>`;
-    });
-    timeZoneList.innerHTML = items;
-  } else if (xhr.readyState === 4 && xhr.status !== 200) {
-    alert("Sorry! The request cannot be fulfilled\nReason:" + xhr.statusText);
+function showTime() {
+  if (ajaxreq.readyState === 4 && ajaxreq.status === 200) {
+    let obj = JSON.parse(ajaxreq.responseText);
+    let current = new Date(obj.datetime);
+    let str = current.toDateString() + "," + current.toLocaleTimeString();
+    let span = document.querySelector("#currentdatetime");
+    span.innerHTML = str;
   }
 }
-function showData() {
-  xhr = new XMLHttpRequest();
-  timeZoneName = timeZoneList.value;
-  let endPoint2 = endPoint + timeZoneName;
-  xhr.onreadystatechange = processDate;
-  xhr.open("GET", endPoint2, true);
-  xhr.send(null);
-}
-function processDate() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    let dateTimeSpan = document.getElementById("currentdatetime");
-    const obj = JSON.parse(xhr.responseText);
-    console.log(obj);
-    let dateString = obj.datetime;
-    let globalDate = new Date(dateString);
-    let str =
-      globalDate.toDateString() +
-      "," +
-      globalDate.toLocaleTimeString("en-US", { timeZone: timeZoneName });
-    dateTimeSpan.innerHTML = str;
-  } else if (xhr.readyState === 4 && xhr.status !== 200) {
-    alert("Sorry! The request cannot be fulfilled\nReason:" + xhr.statusText);
-  }
-}
-window.onload = loadTimeZones;
